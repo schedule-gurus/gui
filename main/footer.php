8a3274f0dd2b48213de3fdf26bd9cf45c70a0922
Pilar Luiz <pluiz@usc.edu>
2020-11-17 13:58:42 -0800

    changes

--- main/footer.php
+++ main/footer.php
@@ -1,4 +1,4 @@
-<div id="footer">
+<div id="footer" class="footer fixed-bottom">
 	<div class="container">
 		<h6>&copy; Nirav Adunuthula, Jaehyung Choi, Sachi Desale, Camden Jones, Kevin Lee, & Pilar Luiz - CSCI 201 Fall 2020: Final Project.</h6>
 	</div>
