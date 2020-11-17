84b8fc0d1a94a60df6559480e14d63eb17c0e303
Pilar Luiz <pluiz@usc.edu>
2020-11-17 14:08:36 -0800

    more changes lmao

--- main/nav.php
+++ main/nav.php
@@ -1,6 +1,6 @@
 
 
-<nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-search navbar-fixed-top" id="navbar">
     <div class="container-fluid"><a class="navbar-brand" href="#">USC Schedule Helper</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="nav navbar-nav">
